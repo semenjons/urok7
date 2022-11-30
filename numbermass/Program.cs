71c7e3d0cf6a8892f4efc17085d45e502e221f72
semen.jons <semen.jons@yandex.ru>
2022-11-30 20:37:49 +0300

    возврат значения найденного числа

---
+++ numbermass/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool Number(int[,] array, int number)
+{
+    bool kondition = false;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == number)
+                kondition  = true;
+        }
+    }
+    return kondition;
+}
+
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов массива: ");
+int cols = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число, которое будем искать в массиве: ");
+int number = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(rows, cols);
+PrintArray(array);
+
+if (Number( array,number) is true)
+    Console.WriteLine($"Число {number} найдено в массиве. ");
+else
+    Console.WriteLine($"Число {number} не найдено в массиве.");
+
+

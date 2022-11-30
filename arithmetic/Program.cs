321c900476ba4a1b06ad9ca7962bea71c76374c6
semen.jons <semen.jons@yandex.ru>
2022-11-30 20:49:45 +0300

    определение среднеарифметического числа в массиве

---
+++ arithmetic/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+double ArithmeticMean(int[,] array, double rows, int cols)
+{
+ double index = 0;
+    for (int i = 0; i < cols; i++){
+        double sum = 0;
+        for (int j = 0; j < rows; j++)
+        { sum += array[j, i];}
+        index = sum / rows;
+        Console.WriteLine($"В столбце {i + 1} среднее арифметическое  = {Math.Round(index, 2)}");
+    }
+    return index;
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов массива: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, cols);
+
+PrintArray(array);
+ArithmeticMean(array, rows, cols);
